05e60403ee8c369e79320395e84199fe4d43d855
Robert DeLuca <robertdeluca19@gmail.com>
2019-06-05 17:21:48 -0500

    Disable deprecated Flow Control & use `async` / `await`
    This also removes the windows test commands since they're not needed anymore.

--- conf.js
+++ conf.js
@@ -8,6 +8,14 @@ exports.config = {
   // Spec files with our tests.
   specs: ['*_spec.js'],
 
+  // Required since we're using `async` / `await`:
+  // "Because async/await uses native promises, it will make the Control Flow
+  // unreliable"
+  // Control Flow is also deprecated
+  // see: https://www.protractortest.org/#/control-flow
+  // see: https://github.com/SeleniumHQ/selenium/issues/2969
+  SELENIUM_PROMISE_MANAGER: false,
+
   // Options for the webdriver instance to use in tests.
   capabilities: {
     browserName: 'chrome',
