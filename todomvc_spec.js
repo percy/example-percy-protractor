b9a12c3c76f37f1ecbdff8764a35b62fddb0d5df
Robert DeLuca <robertdeluca19@gmail.com>
2019-06-06 12:02:52 -0500

    fix: Disable deprecated Flow Control & use `async` / `await` (#35)
    * Disable deprecated Flow Control & use `async` / `await`
    
    This also removes the windows test commands since they're not needed anymore.
    
    * Update to the latest protractor SDK

--- todomvc_spec.js
+++ todomvc_spec.js
@@ -17,40 +17,39 @@ describe('TodoMVC', function() {
     server.close()
   })
 
-  afterEach(function() {
+  afterEach(async function() {
     // Clear local storage between tests so that we always start with a clean slate.
-    browser.executeScript('window.localStorage.clear()')
+    await browser.executeScript('window.localStorage.clear()')
   })
 
-  it('Loads the app', function() {
-    browser.get(TEST_URL)
-    expect(element(by.css('section.todoapp')).isPresent()).toBe(true)
-    percySnapshot("Home page")
+  it('Loads the app', async function() {
+    await browser.get(TEST_URL)
+    expect(await element(by.css('section.todoapp')).isPresent()).toBe(true)
+    await percySnapshot("Home page")
   })
 
-  it('Accepts a new todo', function() {
-    browser.get(TEST_URL)
-    element(by.css('.new-todo')).sendKeys('New fancy todo', protractor.Key.ENTER)
+  it('Accepts a new todo', async function() {
+    await browser.get(TEST_URL)
+    await element(by.css('.new-todo')).sendKeys('New fancy todo', protractor.Key.ENTER)
 
-    element.all(by.css('.todo-list li')).then(function(todos) {
-      expect(todos.length).toBe(1)
-    })
+    const todos = await element.all(by.css('.todo-list li'))
+    expect(todos.length).toBe(1)
 
-    percySnapshot("New todo", { widths: [768, 992, 1200] })
+    await percySnapshot("New todo", { widths: [768, 992, 1200] })
   })
 
-  it('Lets you check off a a todo', function() {
-    browser.get(TEST_URL)
-    element(by.css('.new-todo')).sendKeys('A thing to accomplish', protractor.Key.ENTER)
+  it('Lets you check off a a todo', async function() {
+    await browser.get(TEST_URL)
+    await element(by.css('.new-todo')).sendKeys('A thing to accomplish', protractor.Key.ENTER)
 
-    let itemsLeftText = element(by.css('.todo-count')).getText()
-    expect(itemsLeftText).toBe('1 item left')
+    let itemsLeftText = await element(by.css('.todo-count')).getText()
+    await expect(itemsLeftText).toBe('1 item left')
 
-    element(by.css('input.toggle')).click()
+    await element(by.css('input.toggle')).click()
 
-    itemsLeftText = element(by.css('.todo-count')).getText()
-    expect(itemsLeftText).toBe('0 items left')
+    itemsLeftText = await element(by.css('.todo-count')).getText()
+    await expect(itemsLeftText).toBe('0 items left')
 
-    percySnapshot("Checked-off todo", { widths: [300] })
+    await percySnapshot("Checked-off todo", { widths: [300] })
   })
 })
