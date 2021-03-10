e6bf9457519cabd12c34756d6f2e9c1d8e0592bb
Robert DeLuca <robertdeluca19@gmail.com>
2021-03-10 11:51:13 -0600

    ✨ Upgrade to v2 (#55)
    * ♻️Update example Todo app, update @percy/protractor to v2
    
    * :memo: Update README with tutorial
    
    * ♻ Update CI config
    
    * ♻️Set `browser.waitForAngularEnabled(false)`
    
    The TodoMVC app is no longer an angular app, so that has to be enabled

--- conf.js
+++ conf.js
@@ -1,26 +1,11 @@
 exports.config = {
-  // Connect directly to browser, without intermediate Selenium Server.
   directConnect: true,
-
-  // Test framework to use.
   framework: 'jasmine',
-
-  // Spec files with our tests.
-  specs: ['*_spec.js'],
-
-  // Required since we're using `async` / `await`:
-  // "Because async/await uses native promises, it will make the Control Flow
-  // unreliable"
-  // Control Flow is also deprecated
-  // see: https://www.protractortest.org/#/control-flow
-  // see: https://github.com/SeleniumHQ/selenium/issues/2969
-  SELENIUM_PROMISE_MANAGER: false,
-
-  // Options for the webdriver instance to use in tests.
+  specs: ['test/*.test.js'],
   capabilities: {
-    browserName: 'chrome',
-    chromeOptions: {
-      args: ["--headless"]
+    browserName: 'firefox',
+    'moz:firefoxOptions': {
+      args: ['-headless']
     }
   }
-}
+};
---
+++ test/todomvc.test.js
@@ -0,0 +1,55 @@
+const httpServer = require('http-server');
+const percySnapshot = require('@percy/protractor');
+
+describe('TodoMVC', function () {
+  const PORT = 8000;
+  const TEST_URL = `http://localhost:${PORT}`;
+  let server;
+
+  beforeAll(function () {
+    server = httpServer.createServer({ root: `${__dirname}/../` });
+    server.listen(PORT);
+    browser.waitForAngularEnabled(false);
+  });
+
+  afterAll(function () {
+    // Shutdown our http server.
+    server.close();
+  });
+
+  afterEach(async function () {
+    // Clear local storage between tests so that we always start with a clean slate.
+    await browser.executeScript('window.localStorage.clear()');
+  });
+
+  it('Loads the app', async function () {
+    await browser.get(TEST_URL);
+    expect(await element(by.css('section.todoapp')).isPresent()).toBe(true);
+    await percySnapshot('Home page');
+  });
+
+  it('Accepts a new todo', async function () {
+    await browser.get(TEST_URL);
+    await element(by.css('.new-todo')).sendKeys('New fancy todo', protractor.Key.ENTER);
+
+    const todos = await element.all(by.css('.todo-list li'));
+    expect(todos.length).toBe(1);
+
+    await percySnapshot('New todo', { widths: [768, 992, 1200] });
+  });
+
+  it('Lets you check off a a todo', async function () {
+    await browser.get(TEST_URL);
+    await element(by.css('.new-todo')).sendKeys('A thing to accomplish', protractor.Key.ENTER);
+
+    let itemsLeftText = await element(by.css('.todo-count')).getText();
+    await expect(itemsLeftText).toBe('1 item left');
+
+    await element(by.css('input.toggle')).click();
+
+    itemsLeftText = await element(by.css('.todo-count')).getText();
+    await expect(itemsLeftText).toBe('0 items left');
+
+    await percySnapshot('Checked-off todo', { widths: [300] });
+  });
+});
